ef2b1948ecf2c1aa6abf0cb402b814cb73a8d337
Jhony López Somoza <jhony-bryan@outlook.com>
2019-02-21 22:52:20 -0600

    Se creo la conexion a la base de datos correctamente

---
+++ src/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const mongoose = require("mongoose");
+const app = require("./app");
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/VentaOnline', {useNewUrlParser: true}).then(()=>{
+    console.log('Se encuentra conectado a la base de datos');
+
+    app.set('port', process.env.PORT || 3000);
+    app.listen(app.get('port'), ()=>{
+        console.log(`El servidor esta corriendo en el puerto: '${app.get('port')}'`);
+    });
+}).catch(err => console.log(err));
