8dc995a618f3709dbf19ccdc6a84d2f925806be2
Jhony López Somoza <jhony-bryan@outlook.com>
2019-02-28 23:14:08 -0600

    Se confirman los cambios en el repositorio

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,1 +1,17 @@
 'use strict'
+
+var Category = require('../models/category');
+
+var inicioPrograma = function(primeraTabla) {
+    callBack(primeraTabla)
+}
+
+function primeraTabla(req, res) {
+    var category = new Category();
+
+    category.nombreCategoria = 'Miscelanea';
+}
+
+module.exports = {
+    inicioPrograma
+}
