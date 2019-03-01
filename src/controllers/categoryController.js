11372ec1ef8c89407251dabdc21d1650df1fb91a
programacion <programacion@fkinal.edu>
2019-03-01 09:12:21 -0600

    Se agrego la funcionalidad de crear categorias por el administrador

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -17,7 +17,40 @@ categoriaMiscelanea(function creaMiscelanea(res) {
     })
 });
 
+function crearCategoria(req, res) {
+    var categoria = new Categorias();
+    var params = req.body;
 
+    if(params.nombreCategoria){
+        categoria.nombreCategoria = params.nombreCategoria;
+
+        Categorias.find({ $or: [
+            {nombreCategoria: categoria.nombreCategoria.toLowerCase()},
+            {nombreCategoria: categoria.nombreCategoria.toUpperCase()}
+        ]}).exec((err, categories)=>{
+            if(err) return res.status(500).send({message: 'Error en la peticion'});
+
+            if(categories && categories.length >= 1){
+                return res.status(500).send({message: 'La categoria ya existe en el sistema'});
+            }else{
+                categoria.save((err, categoriaGuardada)=>{
+                    if(err) return res.status(500).send({message: 'Error en la peticion'});
+
+                    if(categoriaGuardada){
+                        res.status(200).send({categoria: categoriaGuardada});
+                    }else{
+                        res.status(404).send({message: 'No se a podido agregar la categoria'});
+                    }
+                })
+            }
+        })
+    }else{
+        res.status(200).send({
+            message: 'Rellene todos los campos necesarios'
+        });
+    }
+}
 module.exports = {
-    categoriaMiscelanea
+    categoriaMiscelanea,
+    crearCategoria
 }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -7,11 +7,14 @@ var md_auth = require('../middlewares/authenticated');
 var administrador = require('../middlewares/esAdmin'); 
 
 var api = express.Router();
+//Usuarios
 
 api.post('/agregar-administrador', UserController.registrar);
 api.post('/registrar', [md_auth.ensureAuth, administrador.esAdmin] ,UserController.registrar);
 api.post('/login', UserController.login);
 
+// Categoria
+api.post('/agregar-categoria', [md_auth.ensureAuth, administrador.esAdmin], CategoryController.crearCategoria);
 
 api.put('/editar-usuario/:id', md_auth.ensureAuth, UserController.editarUsuario);
 
